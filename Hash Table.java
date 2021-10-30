192bea3eccd0d7e8dab0b416d9c0182bda846c2b
Zoro Chen <chendongsong@outlook.com>
2021-10-29 21:39:08 -0700

    Add files via upload

---
+++ Hash Table.java
@@ -0,0 +1,138 @@
+// 560
+class Solution {
+    public int subarraySum(int[] nums, int k) {
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(0, 1);
+        int sum = 0;
+        int res = 0;
+        for (int num : nums) {
+            sum += num;
+            int remain = sum - k;
+            int countRemain = map.getOrDefault(remain, 0);
+            int countSum = map.getOrDefault(sum, 0);
+            map.put(sum, countSum + 1);
+            res += countRemain;
+        }
+        return res;
+    }
+}
+
+// 811
+class Solution {
+    public List<String> subdomainVisits(String[] cpdomains) {
+        Map<String, Integer> map = new HashMap();
+        for (String cd : cpdomains) {
+            int i = cd.indexOf(' ');
+            int n = Integer.valueOf(cd.substring(0, i));
+            String s = cd.substring(i + 1);
+            for (i = 0; i < s.length(); ++i) {
+                if (s.charAt(i) == '.') {
+                    String d = s.substring(i + 1);
+                    map.put(d, map.getOrDefault(d, 0) + n);
+                }
+            }
+            map.put(s, map.getOrDefault(s, 0) + n);
+        }
+
+        List<String> res = new ArrayList();
+        for (String d : map.keySet()) res.add(map.get(d) + " " + d);
+        return res;        
+    }
+}
+
+// 49
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        List<List<String>> res = new ArrayList<>();
+        Map<String, List<String>> map = new HashMap<>();
+        for (String str : strs) {
+            char[] arr = str.toCharArray();
+            Arrays.sort(arr);
+            String key = new String(arr);
+            List<String> list = map.getOrDefault(key, new ArrayList<>());
+            list.add(str);
+            map.put(key, list);
+        }
+        res.addAll(map.values());
+        return res;
+    }
+}
+
+// 981
+class TimeMap {
+
+    private Map<String,TreeMap<Integer,String>> map;
+
+    /** Initialize your data structure here. */
+    public TimeMap() {
+        map = new HashMap<>();
+    }
+
+    public void set(String key, String value, int timestamp) {
+        if(!map.containsKey(key)) {
+            map.put(key,new TreeMap<>());
+        }
+        map.get(key).put(timestamp,value);
+    }
+
+    public String get(String key, int timestamp) {
+        TreeMap<Integer,String> treeMap = map.get(key);
+        if(treeMap==null) {
+            return "";
+        }
+        Integer floor = treeMap.floorKey(timestamp);
+        if(floor==null) {
+            return "";
+        }
+        return treeMap.get(floor);
+    }
+}
+
+
+// 621
+public class Solution {
+    public int leastInterval(char[] tasks, int n) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int maxF = 0;
+        for(char a : tasks){
+            int b = a-'A';
+            map.put(b,map.getOrDefault(b,0)+1);
+            maxF = Math.max(maxF, map.get(b));
+        }
+        int res = (maxF-1)*(n+1);
+        for(int a : map.keySet()){
+            if(map.get(a) ==  maxF) res++;
+        }
+        return Math.max(tasks.length, res);
+    }
+}
+
+// 692
+class Solution {
+    public List<String> topKFrequent(String[] words, int k) {
+        
+        List<String> result = new LinkedList<>();
+        Map<String, Integer> map = new HashMap<>();
+        for (int i=0; i<words.length; i++) {
+            int times = map.getOrDefault(words[i], 0);
+            map.put(words[i], times + 1);
+        }
+        
+        PriorityQueue<Map.Entry<String, Integer>> pq = new PriorityQueue<>(
+                 (a,b) -> a.getValue()==b.getValue() ? 
+                    b.getKey().compareTo(a.getKey()) : a.getValue()-b.getValue()
+        );
+        
+        for (Map.Entry<String, Integer> entry: map.entrySet()) {
+            pq.offer(entry);
+            if (pq.size()>k){
+                pq.poll();
+            }
+        }
+
+        while (!pq.isEmpty())
+            result.add(0, pq.poll().getKey());
+        
+        return result;
+    }
+}
