45de4e102ebc1be3399462feabf87a1a3b9ab49b
Zoro Chen <chendongsong@outlook.com>
2022-02-23 18:32:45 -0800

    Update Queue & BFS.java

--- Queue & BFS.java
+++ Queue & BFS.java
@@ -23,32 +23,6 @@ class MovingAverage {
     }
 }
 
-// 239
-class MovingAverage {
-
-    Queue<Integer> q;
-    int capacity;
-    int currSum;
-
-    /** Initialize your data structure here. */
-    public MovingAverage(int size) {
-        q = new LinkedList<Integer>();
-        capacity = size;
-        currSum = 0;
-    }
-    
-    public double next(int val) {
-        currSum += val;
-        q.offer(val);
-        
-        if(q.size() > capacity ){
-            currSum -= q.poll();
-        }
-       
-        return currSum*1.0 / q.size() ;
-    }
-}
-
 // 102
 class Solution {
     public List<List<Integer>> levelOrder(TreeNode root) {
