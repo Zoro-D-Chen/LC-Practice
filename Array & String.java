b135f4cf286a02aa6f97358c9643e200aa24b5ad
Zoro Chen <chendongsong@outlook.com>
2023-07-07 20:01:25 -0700

    Update Array & String.java

--- Array & String.java
+++ Array & String.java
@@ -5,10 +5,10 @@ class Solution {
             return true;
         }
         
-        Arrays.sort(intervals, (i1, i2) -> (i1.start - i2.start));
+        Arrays.sort(intervals, (i1, i2) -> (i1[0] - i2[0]));
         
         for (int i = 1; i < intervals.length; i++) {
-            if (intervals[i].start < intervals[i - 1].end) {
+            if (intervals[i][0] < intervals[i - 1][1]) {
                 return false;
             }
         }
